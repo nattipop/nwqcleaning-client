300ecb863be5257263a6e6b4af9afc05f3b1d32d
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-08-22 15:30:26 -0500

    app.css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-import './index.css';
+import './App.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
