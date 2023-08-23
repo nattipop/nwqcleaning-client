0fc76a0ca235d4efdfa484876d04517740073e8b
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-08-22 20:26:00 -0500

    photoset with cloudinary

--- src/Pictures.js
+++ src/Pictures.js
@@ -1,6 +1,24 @@
+import photos from "./photosets.json"
+
 const Pictures = () => {
+  const renderPhotos = () => {
+    return photos.map((photoset) => {
+      return (
+        <div key={photoset.id}>
+          <div className="row">
+            <img className="col photoset-img" src={photoset.before_url} alt="" />
+            <img className="col photoset-img" src={photoset.after_url} alt="" />
+          </div>
+          <h2>{photoset.quote_text}</h2>
+        </div>
+      )
+    })
+  }
   return (
-    <div id="pictures">pictures</div>
+    <div id="pictures">
+      {renderPhotos()}
+      pictures
+    </div>
   )
 }
 
