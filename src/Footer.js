a13a90f446666e1fdb7af866c78d45de0122c980
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-09-17 20:45:21 -0500

    footer styles

--- src/Footer.js
+++ src/Footer.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <div id="footer">
@@ -9,6 +11,17 @@ const Footer = () => {
           <h1>Contact</h1>
           <h3>(715) 651-8994</h3>
           <h3>nwqcleaning@gmail.com</h3>
+        </div>
+        <div className="col-2 icons-div">
+          <Link id="instagram-icon" to="https://www.instagram.com/n.w.q_cleaning/">
+            <img className="icons" src="https://cdn-icons-png.flaticon.com/512/1384/1384031.png" alt="" />
+          </Link>
+          <Link id="facebook-icon" to="https://www.facebook.com/NWQCleaning">
+            <img className="icons" src="https://cdn3.iconfinder.com/data/icons/picons-social/57/46-facebook-512.png" alt="" />
+          </Link>
+          <Link id="google-icon" to="https://www.google.com/maps/@45.3154442,-91.6658582,15z?entry=ttu">
+            <img className="icons" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Map_marker_font_awesome.svg/512px-Map_marker_font_awesome.svg.png" alt="" />
+          </Link>
         </div>
       </div>
     </div>
--- src/Home.js
+++ src/Home.js
@@ -11,12 +11,8 @@ const Home = () => {
   return (
     <div id="home">
       <div id="hero">
-        <img src="https://rrpwi.com/wp-content/uploads/2022/06/river-pic.jpg" alt="" />
         <h1 id="hero-text">Northwest Quality Cleaning</h1>
       </div>
-      {/* <div id="hero-logo">
-        <img src="https://res.cloudinary.com/dawteptkh/image/upload/v1692930718/IMG_1955_Small_a86c79.jpg" alt="" />
-      </div> */}
       <div id="instagram-section"></div>
     </div>
   )
