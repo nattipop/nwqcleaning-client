a02adfe357d3346548413d2344b78e7e75b14945
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-09-22 09:47:08 -0500

    our purpose styling

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,10 @@
 import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 const reviewsWidget = document.getElementById("reviews-widget");
 
 const Home = () => {
+  const navigate = useNavigate();
   useEffect(() => {
     document.title = "Northwest Quality Cleaning | Home";
   // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -12,6 +14,18 @@ const Home = () => {
     <div id="home">
       <div id="hero">
         <h1 id="hero-text">Northwest Quality Cleaning</h1>
+      </div>
+      <div id="pictures-reviews">
+        <div className="row pic-rev">
+            <div className="col pic-rev-div" onClick={() => navigate("/pictures")}>
+              <img className="pic-rev-photo" src="https://res.cloudinary.com/dawteptkh/image/upload/v1693372589/IMG_8946_wwasxt.jpg" width="500px" alt="" />
+              <h2 className="pic-rev-text">Pictures</h2>
+            </div>
+            <div className="col pic-rev-div" onClick={() => navigate("/reviews")}>
+              <img className="pic-rev-photo" src="https://cdn-icons-png.flaticon.com/512/2701/2701190.png" width="300px" alt="" />
+              <h2 className="pic-rev-text">Reviews</h2>
+            </div>
+        </div>
       </div>
       <div id="instagram-section"></div>
     </div>
--- src/WhoWeAre.js
+++ src/WhoWeAre.js
@@ -6,13 +6,13 @@ const WhoWeAre = () => {
   }, [])
   return (
     <div id="who-we-are">
-      <div id="wwa-header-div" className="row">
-        <div id="wwa-text-background" className="col-6">
-          <h1 className="wwa-header">Hi! We're Kristin & Domanic Wiesner,</h1>
-          <h1 className="wwa-sub-header">the owners of Northwest Quality Cleaning LLC. We're so grateful to get to work together as husband and wife, with this being our full time career, and supporting us as we work towards building our future together, all by God's grace!</h1>
-        </div>
-        <div className="col-6">
-          <img className="wwa-picture" src="https://res.cloudinary.com/dawteptkh/image/upload/v1693497661/Krisitn_Dom_sl2wak.jpg" alt="" />
+      <div id="wwa-header-div">
+        <div id="wwa-text-background">
+          <div id="kristin_and_dom">
+            <img src="https://res.cloudinary.com/dawteptkh/image/upload/v1693497661/Krisitn_Dom_sl2wak.jpg" alt="" />
+          </div>
+          <h1 className="wwa-header about-text-header">We're Kristin & Domanic Wiesner,</h1>
+          <h1 className="wwa-sub-header about-text-sub">the owners of Northwest Quality Cleaning LLC. We're so grateful to get to work together as husband and wife, with this being our full time career, and supporting us as we work towards building our future together, all by God's grace!</h1>
         </div>
       </div>
       <div id="wwa-our-purpose" className="row">
@@ -21,7 +21,7 @@ const WhoWeAre = () => {
         </div>
         <div id="our-purpose-text" className="col-6">
           <h1 className="wwa-header">Our Purpose:</h1>
-          <h1 className="wwa-sub-header">We're here to serve you, make your life easier, take the time to do the things you don't have time for, or maybe the capability to do anymore.We are dedicated to our quality towards every client, whether a one time or continual weekly cleaning, we will serve you and your home to the best of our abilities, giving you confidence and peace of mind, with your decision to take us on as your personal cleaners!</h1>
+          <h1 className="wwa-sub-header">We're here to serve you, make your life easier, and take the time to do the things you don't have time or maybe the capability to do anymore. We are dedicated to our quality towards every client, whether a one time or continual weekly cleaning, we will serve you and your home to the best of our abilities, giving you confidence and peace of mind, with your decision to take us on as your personal cleaners!</h1>
         </div>
       </div>
     </div>
